f6bcd85fbae3fc634e7214534e702197694ece70
mln-i <mln-i@hotmail.com>
2018-10-31 12:58:00 +0100

    Add Result component

--- src/main/java/hva/ewa/Entrepreneurship/controller/ResultController.java
+++ src/main/java/hva/ewa/Entrepreneurship/controller/ResultController.java
@@ -1,12 +1,22 @@
 package hva.ewa.Entrepreneurship.controller;
 
+import hva.ewa.Entrepreneurship.model.Result;
+import hva.ewa.Entrepreneurship.model.User;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/result")
+@RequestMapping("/api")
 public class ResultController {
 
+    @RequestMapping(method = RequestMethod.GET, value = "/result")
+    public List<User> getResults(Result result) {
+
+        return null;
+    }
 }
--- src/main/java/hva/ewa/Entrepreneurship/model/Result.java
+++ src/main/java/hva/ewa/Entrepreneurship/model/Result.java
@@ -1,13 +1,11 @@
 package hva.ewa.Entrepreneurship.model;
 
+import javax.persistence.*;
+
+@Entity // This tells Hibernate to make a table out of this class
+@Table(name="result")
 public class Result {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer result_id;
-    private String competence1;
-    private String competence2;
-    private String competence3;
-    private String competence4;
-    private String competence5;
-    private String subcompetence1;
-    private String subcompetence2;
-    private String subcompetence3;
 }
