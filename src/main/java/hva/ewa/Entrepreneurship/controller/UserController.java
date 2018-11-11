995504fc71728ddf7e9d826fe78a46cf4ed990e8
mln-i <mln-i@hotmail.com>
2018-11-11 14:12:03 +0100

    alter responses so that it returns http status

--- src/main/java/hva/ewa/Entrepreneurship/controller/UserController.java
+++ src/main/java/hva/ewa/Entrepreneurship/controller/UserController.java
@@ -5,6 +5,8 @@ import hva.ewa.Entrepreneurship.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,44 +19,74 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-//    @RequestMapping(method = RequestMethod.GET, value = "/json")
-//    public String returnJSON() {
-//
-//        String hello = "hello";
-//
-//        return hello;
+//    @Autowired
+//    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    //Can be used to populate list of users for admin
+//    public ResponseEntity<List<User>> retrieveAllUsers() {
+//        List<User> users;
 //    }
 
+    @RequestMapping(method = RequestMethod.POST, value = "/users")
+    public ResponseEntity<Void> createUser(@RequestBody User user) {
+
+        if (userRepository.doesUserExist(user.getEmail())) {
+            return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+        }
+        //user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/users/update/{userid}")
+    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable("userid") Integer id) {
+
+        User currentUser = userRepository.findUserById(id);
+
+        if (currentUser == null) {
+            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+        }
+        currentUser.setFirstname(user.getFirstname());
+        currentUser.setLastname(user.getLastname());
+        currentUser.setEmail(user.getEmail());
+        userRepository.updateUser(user.getFirstname(), user.getLastname(), user.getEmail(), id);
+        return new ResponseEntity<User>(HttpStatus.OK);
+    }
+
+    //replaced by createUser
     @RequestMapping(method = RequestMethod.POST, value = "/users/update")
     public ResponseEntity<String> getUser(@RequestBody User user) {
 
-//        System.out.println("User: " + user.getFirstname() + ", " + user.getLastname() + ", " + user.getEmail() + ", " + user.getPassword());
-//        user.setFirstname(user.getFirstname());
-//        user.setLastname(user.getLastname());
-//        user.setEmail(user.getEmail());
-//        user.setPassword(user.getPassword());
-//        user.setRole(user.getRole());
-//        user.setTeacher(user.getTeacher());
         userRepository.save(user);
-        //return new ResponseEntity<>("Status is OK", HttpStatus.OK);
         return new ResponseEntity<>(HttpStatus.OK);
 
     }
 
+    //Can be removed
     @RequestMapping(method = RequestMethod.GET, value = "/users")
     public List<User> existingUser(User user) {
-
-//        System.out.println(userRepository.findAllUsers(user.getEmail(), user.getPassword(), user.getRole()));
 
         return userRepository.findAllUsers(user.getEmail(), user.getPassword(), user.getRole());
     }
 
+    //Can be removed
     @RequestMapping(method = RequestMethod.GET, value = "/users/emails")
     public List<User> existingEmail(User user) {
 
-//        System.out.println(userRepository.findAllEmail(user.getEmail()));
-
         return userRepository.findAllEmail(user.getEmail());
     }
+
+//    @GetMapping("/enterStudent")
+//    @PreAuthorize("hasAnyRole('Student')")
+//    public ResponseEntity<?> enterAsStudent() {
+//        return new ResponseEntity<>("You are student", HttpStatus.OK);
+//    }
+//
+//    @GetMapping("/enterTeacher")
+//    @PreAuthorize("hasAnyRole('Teacher')")
+//    public ResponseEntity<?> enterAsTeacher() {
+//        System.out.println("Responded teacher");
+//        return new ResponseEntity<>("You are teacher", HttpStatus.OK);
+//    }
 
 }
