edd1ce85a7191177a9231fbe02475f8519f60f84
Unknown <namtphann@gmail.com>
2018-11-14 23:18:46 +0100

    Saving khan academy videos to database works now

--- src/main/java/hva/ewa/Entrepreneurship/controller/KhanAcademyController.java
+++ src/main/java/hva/ewa/Entrepreneurship/controller/KhanAcademyController.java
@@ -3,13 +3,11 @@ package hva.ewa.Entrepreneurship.controller;
 import hva.ewa.Entrepreneurship.model.KhanAcademyVideo;
 import hva.ewa.Entrepreneurship.repository.KhanAcademyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -24,42 +22,26 @@ public class KhanAcademyController {
         this.khanAcademyRepository = khanAcademyRepository;
     }
 
-//    @RequestMapping(method = RequestMethod.POST, value = "/khanacademy/video")
-//    public KhanAcademyVideo saveVideos(@RequestBody KhanAcademyVideo khanAcademyVideo) {
-//            khanAcademyRepository.save(khanAcademyVideo);
-//
-//        return khanAcademyVideo;
-//    }
+    @RequestMapping(method = RequestMethod.POST, value = "/khanacademy/video")
+    public ResponseEntity insertVideos(@RequestBody KhanAcademyVideo[] khanAcademyVideo){
 
-//    @RequestMapping(method = RequestMethod.POST, value = "/results/testscore")
-//    public Result insertScore(@RequestBody Result result) {
-//
-//        resultRepository.save(result);
-//        return result;
-//    }
+        for (KhanAcademyVideo video : khanAcademyVideo) {
+            khanAcademyRepository.save(video);
+        }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/khanacademy/video", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity insertVideos(@RequestBody KhanAcademyVideo khanAcademyVideo){
-
-//        List<KhanAcademyVideo> list = new ArrayList<>();
-//
-//        for (KhanAcademyVideo video : khanAcademyVideo) {
-//            System.out.println(khanAcademyVideo);
-//            list.add(video);
-            khanAcademyRepository.save(khanAcademyVideo);
-//        }
-
-//        if (list.isEmpty()) {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
-        return new ResponseEntity<>(HttpStatus.OK);
+        if (khanAcademyVideo.length == 0) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(khanAcademyVideo,HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/khanacademy/videolist")
-    public List<KhanAcademyVideo> videoList(KhanAcademyVideo khanAcademyVideo) {
+    public ResponseEntity videoList(KhanAcademyVideo khanAcademyVideo) {
 
-        return khanAcademyRepository.getAllVideos(khanAcademyVideo.getId(), khanAcademyVideo.getInternal_id(), khanAcademyVideo.getTitle(),
+        List<KhanAcademyVideo> khanAcademyVideoList = khanAcademyRepository.getAllVideos(khanAcademyVideo.getId(), khanAcademyVideo.getInternal_id(), khanAcademyVideo.getTitle(),
                 khanAcademyVideo.getDescription(), khanAcademyVideo.getIcon(), khanAcademyVideo.getIcon_large(),
                 khanAcademyVideo.getUrl(), khanAcademyVideo.getShow_on_top(), khanAcademyVideo.getShow_hide());
+
+        return new ResponseEntity<>(khanAcademyVideoList,HttpStatus.OK);
     }
 }
--- src/main/java/hva/ewa/Entrepreneurship/model/KhanAcademyVideo.java
+++ src/main/java/hva/ewa/Entrepreneurship/model/KhanAcademyVideo.java
@@ -2,31 +2,38 @@ package hva.ewa.Entrepreneurship.model;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity // This tells Hibernate to make a table out of this class
 @Table(name = "khanacademy")
 public class KhanAcademyVideo {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     @NotNull
+    @Size(max = 100)
     private String internal_id;
 
     @NotNull
+    @Size(max = 300)
     private String title;
 
     @NotNull
+    @Size(max = 1000)
     private String description;
 
     @NotNull
+    @Size(max = 300)
     private String icon;
 
     @NotNull
+    @Size(max = 300)
     private String icon_large;
 
     @NotNull
+    @Size(max = 300)
     private String url;
 
     @NotNull
