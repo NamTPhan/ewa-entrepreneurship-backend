07b2348b38fbef8780db02ee3f36b5fac47cf9f3
Unknown <namtphann@gmail.com>
2018-12-31 23:46:39 +0100

    Added teacher update user function

--- src/main/java/hva/ewa/Entrepreneurship/controller/UserController.java
+++ src/main/java/hva/ewa/Entrepreneurship/controller/UserController.java
@@ -53,6 +53,20 @@ public class UserController {
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
+    @PutMapping(value = "/users/user/{userid}")
+    public ResponseEntity<User> updateUser(@RequestBody User updateUser, @PathVariable("userid") Integer userid) {
+
+        User user = userRepository.findUserById(userid);
+
+        user.setEmail(updateUser.getEmail());
+        user.setFirst_name(updateUser.getFirst_name());
+        user.setLast_name(updateUser.getLast_name());
+        user.setRole(updateUser.getRole());
+
+        userRepository.save(user);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
     //Can be removed
     @RequestMapping(method = RequestMethod.GET, value = "/users/emails")
     public List<User> existingEmail(User user) {
