b226ea390034458763408940223bcc0aa0f71eb4
mln-i <mln-i@hotmail.com>
2019-01-05 15:43:31 +0100

    add method and query to update Class

--- src/main/java/hva/ewa/Entrepreneurship/controller/ClassController.java
+++ src/main/java/hva/ewa/Entrepreneurship/controller/ClassController.java
@@ -1,6 +1,7 @@
 package hva.ewa.Entrepreneurship.controller;
 
 import hva.ewa.Entrepreneurship.model.Class;
+import hva.ewa.Entrepreneurship.model.User;
 import hva.ewa.Entrepreneurship.repository.ClassRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,6 +18,7 @@ public class ClassController {
     @Autowired
     private ClassRepository classRepository;
 
+    //create new class
     @RequestMapping(method = RequestMethod.POST, value = "/class")
     public ResponseEntity createClass(@RequestBody Class createdClass) {
 
@@ -29,13 +31,21 @@ public class ClassController {
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
+    //update class
     @RequestMapping(method = RequestMethod.PUT, value = "/class/{class_id}")
     public ResponseEntity updateClassOfUser(@RequestBody Class createdClass, @PathVariable("class_id") Integer class_id) {
 
         Class currentClass = classRepository.findClassById(class_id);
+        List<User> listOfUsersInCurrentClass = classRepository.findUserByClass(currentClass.getClass_name());
 
         if (currentClass == null) {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+        //update any students belonging to class that is updated
+        for (int i = 0; i < listOfUsersInCurrentClass.size(); i++) {
+            listOfUsersInCurrentClass.get(i).setClass_name(createdClass.getClass_name());
+            classRepository.updateClassOfUser(createdClass.getClass_name(), listOfUsersInCurrentClass.get(i).getId());
         }
 
         currentClass.setClass_name(createdClass.getClass_name());
@@ -44,6 +54,7 @@ public class ClassController {
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    //get a list of class from specific teacher
     @RequestMapping(method = RequestMethod.GET, value = "/class/list/teacher/{user_id}")
     public ResponseEntity getAllClassesOfTeacher(@PathVariable("user_id") Integer user_id) {
 
--- src/main/java/hva/ewa/Entrepreneurship/repository/ClassRepository.java
+++ src/main/java/hva/ewa/Entrepreneurship/repository/ClassRepository.java
@@ -2,6 +2,7 @@ package hva.ewa.Entrepreneurship.repository;
 
 
 import hva.ewa.Entrepreneurship.model.Class;
+import hva.ewa.Entrepreneurship.model.User;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -22,9 +23,17 @@ public interface ClassRepository extends CrudRepository<Class, Integer> {
     @Query("SELECT c FROM Class c WHERE c.user_id = ?1")
     List<Class> listAllClasses(@PathVariable("user_id") Integer user_id);
 
+    @Query("SELECT u FROM User u WHERE u.class_name = ?1")
+    List<User> findUserByClass(String class_name);
+
     @Transactional
     @Modifying
     @Query("UPDATE Class c SET c.class_name = ?1 WHERE c.class_id = ?2")
     Integer updateClass(String class_name, @PathVariable("class_id") Integer class_id);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE User u SET u.class_name = ?1 WHERE u.id = ?2")
+    Integer updateClassOfUser(String class_name, Integer id);
 
 }
